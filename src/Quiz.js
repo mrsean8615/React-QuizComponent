be15280272857ffc85a7a131098890d3324ee741
mresean8615 <seanpayne479@gmail.com>
2019-03-29 09:51:04 -0600

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -19,7 +19,7 @@ class Quiz extends Component {
         return(
             <div>
                 {isQuizEnd ? <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestion={this.showNextQuestion.bind(this)} />}
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
         )
     }
